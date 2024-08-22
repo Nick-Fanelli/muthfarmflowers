fb53d3dae142352d01467db55e15775ca983e80e
Nick Fanelli <nickfanelli5@gmail.com>
2024-08-21 20:15:28 -0400

    chore: change yellow to salmon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
       {
         default: {
           "primary": "#006fff",
-          "secondary": "#fce579",
+          "secondary": "#ffc891",
           "secondary-content": "#002e00",
           "accent": "#a20000",
           "neutral": "#0d0d0d",
