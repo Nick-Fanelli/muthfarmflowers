d2cd8fed5a841163e3521bd532bf3e73dcde5058
Nick Fanelli <nickfanelli5@gmail.com>
2024-08-24 21:51:01 -0400

    feat: read dir

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -2,16 +2,23 @@ import { Metadata } from "next";
 import Navbar from "../(components)/Navbar";
 import Footer from "../(components)/Footer";
 
+import fs from 'fs'
+import path from "path";
+
 export const metadata: Metadata = {
     title: "Gallery | Muth Farm Flowers"
 };
 
 const Gallery = () => {
 
+    const files = fs.readdirSync(path.join('public', 'gallery'));
+
     return (
 
         <>
             <Navbar selectedElementIndex={2} />
+
+            <h1>{files}</h1>
 
             <Footer />
         </>
